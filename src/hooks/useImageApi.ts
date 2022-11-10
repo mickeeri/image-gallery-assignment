ec2d28d637c246cd9077a307e5bf0270a193d164
mikael-eriksson1 <mikael.a.eriksson@skf.com>
2022-11-10 15:27:41 +0100

    Increase images fetched per page

--- src/hooks/useImageApi.ts
+++ src/hooks/useImageApi.ts
@@ -17,7 +17,12 @@ const ImageListSchema = z.array(ImageSchema);
 const jsonParse = (res: Response) => res.json();
 
 function makeFetchRequest() {
-  return fetch("https://api.unsplash.com/photos", {
+  const url = new URL("https://api.unsplash.com/photos");
+  const params = new URLSearchParams({ per_page: "20" }).toString();
+
+  url.search = params;
+
+  return fetch(url, {
     headers: {
       Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
       "Accept-Version": "v1",
