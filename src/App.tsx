6c9bacf3f6c775cb7d4b7d52f6b79370bdf802c4
mikael-eriksson1 <mikael.a.eriksson@skf.com>
2022-11-10 11:19:08 +0100

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
