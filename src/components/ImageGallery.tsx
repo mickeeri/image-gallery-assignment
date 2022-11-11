e0d1afb65894ae747cad5072f0d8b324e4241a38
mikael-eriksson1 <mikael.a.eriksson@skf.com>
2022-11-11 13:27:52 +0100

    Some more styling

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -3,8 +3,11 @@ import { useImageApi } from "../hooks/useImageApi";
 import { useInView } from "react-intersection-observer";
 
 function ImageGallery() {
-  const { data, isLoading, isError, fetchNextPage } = useImageApi();
+  const { data, isLoading, isError, fetchNextPage, isFetchingNextPage } =
+    useImageApi();
+
   const { ref, inView } = useInView();
+  const showLoader = isLoading || isFetchingNextPage;
 
   useEffect(() => {
     if (inView) {
@@ -14,16 +17,13 @@ function ImageGallery() {
 
   return (
     <div className="mt-8 mx-8 md:mx-0">
-      {isLoading ? "loading" : null}
-
-      {/* <ul className="sm:columns-2 md:columns-3 lg:columns-4 gap-6"> */}
-      <ul className="grid sm:columns-2 md:columns-3 lg:grid-cols-4 gap-6">
+      <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {data?.pages.map((page, i) => (
           <React.Fragment key={i}>
             {page.images.map(({ id, urls, altDescription }) => (
-              <li key={id}>
+              <li className="h-72 md:h-64" key={id}>
                 <img
-                  className="w-full aspect-auto rounded"
+                  className="w-full aspect-auto rounded object-cover h-full border border-teal-200"
                   src={urls.small}
                   alt={altDescription || ""}
                 />
@@ -33,15 +33,17 @@ function ImageGallery() {
         ))}
       </ul>
 
-      <footer className="text-center mb-10">
-        {isError && !isLoading ? (
+      <footer className="text-center my-10">
+        {showLoader ? "Loading images..." : null}
+
+        {isError && !showLoader ? (
           <p role="alert" className="text-red-700">
             There was an error loading more images!{" "}
             <button onClick={() => fetchNextPage()}>Try again</button>
           </p>
         ) : null}
 
-        {!isError && !isLoading ? (
+        {!isError && !showLoader ? (
           <button ref={ref} onClick={() => fetchNextPage()}>
             Load more
           </button>
